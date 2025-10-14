04172ca496c10b905570ca7eeac1b870b347ca8d
khadeem100 <khadeem.smart@outlook.com>
2025-10-14 23:17:50 +0200

    editfiz

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ['luminar-edu.nl'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://luminar-edu.nl',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://luminar-edu.nl http://luminar-edu.nl",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
